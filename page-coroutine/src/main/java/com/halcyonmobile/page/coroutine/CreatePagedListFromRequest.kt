7c0acd84131373fef3d5dcd275902e103db6703e
Gergely Hegedus <gergo@halcyonmobile.com>
2019-09-19 10:54:12 +0300

    Add additional kotlin doc and release 0.1.0.2 with it

--- page-coroutine/src/main/java/com/halcyonmobile/page/coroutine/CreatePagedListFromRequest.kt
+++ page-coroutine/src/main/java/com/halcyonmobile/page/coroutine/CreatePagedListFromRequest.kt
@@ -13,19 +13,30 @@ import com.halcyonmobile.page.ProvideCacheByKey
 import com.halcyonmobile.page.ProvideCacheByKeyAndPageSizeState
 import com.halcyonmobile.page.StateProvidingListDataSourceFactory
 import kotlinx.coroutines.CoroutineScope
-import kotlin.reflect.KClass
 
 /**
  * Helper function which sets up your [PagedResult] with coroutine based [DataSourceUpdateListener] & [SuspendProvideDataByPagedKeyAndSize]
+ *
+ * @param coroutineScope in which [request] will be run and [ChannelBasedDataSourceUpdateListener] updated
+ * @param initialPageKey is the key which will be first provided to the [request]
+ * @param request the requests to get the next page, it returns the list of values and the next key.
+ * @param channelBasedDataSourceUpdateListener request state update listener, it's automatically updated while the requests are running / failing / succeeding etc.
+ * @param dataSourceInvalidator a way to invalidate the latest datasource.
+ * @param cache a cache which used by the datasource to gather local values
+ *
+ * The dataSource works the following way:
+ *  - is there a cache? there is, then return that
+ *  - no cache? call the [request]
+ *  - depending on the response of the [request] update the [channelBasedDataSourceUpdateListener] and send the next page
  */
 inline fun <Key, Value, reified Error : Throwable> createPagedResultFromRequest(
     coroutineScope: CoroutineScope,
     initialPageKey: Key,
-    dataSourceInvalidator: DataSourceInvalidator<Key, Value>?,
     crossinline request: suspend (Key, Int) -> Pair<List<Value>, Key>,
-    crossinline cache: (Key, Int) -> Pair<List<Value>, Key>?
+    channelBasedDataSourceUpdateListener: ChannelBasedDataSourceUpdateListener<Error> = ChannelBasedDataSourceUpdateListener(),
+    dataSourceInvalidator: DataSourceInvalidator<Key, Value>? = null,
+    crossinline cache: (Key, Int) -> Pair<List<Value>, Key>? = { _, _ -> null }
 ): PagedResult<Key, Value, Error> {
-    val channelBasedDataSourceUpdateListener = ChannelBasedDataSourceUpdateListener<Error>()
     val dataSourceFactory = StateProvidingListDataSourceFactory<Key, Value, Error>(
         initialPageKey = initialPageKey,
         provideCacheByKey = ProvideCacheByKey { key, size -> cache(key, size) },
@@ -39,54 +50,45 @@ inline fun <Key, Value, reified Error : Throwable> createPagedResultFromRequest(
     )
 }
 
+/**
+ * Helper function which sets up your [PagedResult] with coroutine based [DataSourceUpdateListener] & [SuspendProvideDataByPagedKeyAndSize]
+ *
+ * @param coroutineScope in which [request] will be run and [ChannelBasedDataSourceUpdateListener] updated
+ * @param initialPageKey is the key which will be first provided to the [request]
+ * @param request the requests to get the next page, it returns the list of values and the next key.
+ * @param channelBasedDataSourceUpdateListener request state update listener, it's automatically updated while the requests are running / failing / succeeding etc.
+ * @param dataSourceInvalidator a way to invalidate the latest datasource.
+ * @param cache a cache which used by the datasource to gather local values
+ *
+ * The dataSource works the following way:
+ *  - is there a cache? there is, then return that
+ *  - no cache? call the [request]
+ *  - depending on the response of the [request] update the [channelBasedDataSourceUpdateListener] and send the next page
+ *
+ *  Note: it's different from [createPagedResultFromRequest] in only the cache.
+ *  Now it can return [ProvideCacheByKey.Result.CacheWithRefreshing]
+ *  In this case the cache can return some values while still showing loading and gathering more, in this case when your request finished you should invalidate and return [ProvideCacheByKey.Result.Cache]
+ *  The other cases are similar:
+ *  [ProvideCacheByKey.Result.Cache] means no refreshing happens only the chached value is added
+ *  [ProvideCacheByKey.Result.Null] means the is no cache, the [request] will be called.
+ */
 inline fun <Key, Value, reified Error : Throwable> createPagedResultFromRequestWithStateProvideingCache(
     coroutineScope: CoroutineScope,
     initialPageKey: Key,
-    dataSourceInvalidator: DataSourceInvalidator<Key, Value>?,
     crossinline request: suspend (Key, Int) -> Pair<List<Value>, Key>,
-    crossinline cache: (Key, Int) -> ProvideCacheByKey.Result<Key, Value>
+    crossinline cache: (Key, Int) -> ProvideCacheByKey.Result<Key, Value>,
+    dataSourceInvalidator: DataSourceInvalidator<Key, Value>?,
+    channelBasedDataSourceUpdateListener: ChannelBasedDataSourceUpdateListener<Error> = ChannelBasedDataSourceUpdateListener()
 ): PagedResult<Key, Value, Error> {
-    val channelBasedDataSourceUpdateListener = ChannelBasedDataSourceUpdateListener<Error>()
-    val dataSourceFactory = StateProvidingListDataSourceFactory(
+    val dataSourceFactory = StateProvidingListDataSourceFactory<Key, Value, Error>(
         initialPageKey = initialPageKey,
-        provideCacheByKey = ProvideCacheByKeyAndPageSizeState(cache),
-        provideDataByPageKeyAndSize = SuspendProvideDataByPagedKeyAndSize(coroutineScope, request),
+        provideCacheByKey = ProvideCacheByKeyAndPageSizeState { key, size -> cache(key, size) },
+        provideDataByPageKeyAndSize = SuspendProvideDataByPagedKeyAndSize(coroutineScope) { key, size -> request(key, size) },
         dataSourceUpdateListener = channelBasedDataSourceUpdateListener
     )
     return PagedResult(
         boundaryCallback = null,
-        dataSourceFactory = dataSourceInvalidator?.let {
-            DataSourceAggregatingDataSourceFactory(
-                dataSourceFactory,
-                it
-            )
-        }
-            ?: dataSourceFactory,
+        dataSourceFactory = dataSourceInvalidator?.let { DataSourceAggregatingDataSourceFactory(dataSourceFactory, it) } ?: dataSourceFactory,
         stateChannel = channelBasedDataSourceUpdateListener.stateChannel
     )
 }
-
-inline fun <Key, Value, reified Error : Throwable> createPagedResultFromRequest(
-    coroutineScope: CoroutineScope,
-    initialPageKey: Key,
-    crossinline request: suspend (Key, Int) -> Pair<List<Value>, Key>,
-    invalidator: DataSourceInvalidator<Key, Value>
-): PagedResult<Key, Value, Error> = createPagedResultFromRequest(
-    coroutineScope = coroutineScope,
-    initialPageKey = initialPageKey,
-    dataSourceInvalidator = invalidator,
-    request = {key, size -> request(key, size) },
-    cache = { _, _ -> null }
-)
-
-inline fun <Key, Value, reified Error : Throwable> createPagedResultFromRequest(
-    coroutineScope: CoroutineScope,
-    initialPageKey: Key,
-    crossinline request: suspend (Key, Int) -> Pair<List<Value>, Key>
-): PagedResult<Key, Value, Error> = createPagedResultFromRequest(
-    coroutineScope = coroutineScope,
-    initialPageKey = initialPageKey,
-    dataSourceInvalidator = null,
-    request = {key, size -> request(key, size) },
-    cache = { _, _ -> null }
-)
